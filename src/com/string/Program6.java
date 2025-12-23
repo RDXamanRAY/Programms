02db45068ceb50d94658d88aca2a56894154ba4d
AbhiRay <abhiray11032003@gmail.com>
2025-12-23 05:35:38 +0530

    Program 6

---
+++ src/com/string/Program6.java
@@ -0,0 +1,23 @@
+//Reverse the given String words along with the sentence also
+
+
+package com.string;
+
+public class Program6 {
+    public static String reverseString(String s1){
+        String[] arr = s1.split(" ");
+        String rev = "";
+
+        for (int i= arr.length-1; i>=0; i--){
+            rev += arr[i]+" ";
+        }
+
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "java ia OOPS language";
+        String res = reverseString(s1);
+        System.out.println(res);
+    }
+}
